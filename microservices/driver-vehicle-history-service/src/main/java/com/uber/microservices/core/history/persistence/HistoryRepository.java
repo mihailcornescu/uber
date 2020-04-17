b37ded4489cc16826aaed33d480bfdd5eb3f4338
mihail.cornescu <cmihail06>
2020-04-17 08:46:01 +0300

    driver-history-service deleted

--- microservices/driver-vehicle-history-service/src/main/java/com/uber/microservices/core/history/persistence/HistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.uber.microservices.core.history.persistence;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
-
-public interface HistoryRepository extends ReactiveCrudRepository<HistoryEntity, String> {
-    Flux<HistoryEntity> findByDriverId(int driverId);
-}
